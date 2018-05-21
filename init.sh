75d781142d67dfa67260e1be0f1919ff44495dc1
Sim <simon@s-m-y.com>
2018-05-21 21:31:37 +0300

    no message

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-export PYTHONPATH=''
-source ~/.virtualenvs/fuitdb2/bin/activate
+#export PYTHONPATH=''
+#source ~/.virtualenvs/fuitdb2/bin/activate
+apt-get update
+apt-get install -y unzip
 pip install -U -r requirements.txt
+cp data/fruits-360_dataset_2018_02_08.zip input/
+cd data
+unzip fruits-360_dataset_2018_02_08.zip
+cd ..
+python classify_fruits.py
